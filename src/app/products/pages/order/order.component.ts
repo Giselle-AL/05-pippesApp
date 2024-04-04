ee4dac3d163ca69782a7dca976735efbe30dff7e
Giselle <ABASLEALGIS@GMAIL.COM>
2024-04-04 16:58:17 -0600

    correcciones

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Color, Hero } from '../../interfaces/hero.innterface';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-order',
